d045cc06cf1c902aec45fb7ae07133550b280a14
Vivek Dhiman <vivek@cliqz.com>
2016-03-25 00:49:41 +0100

    added xcode project

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  hello.cpp
+//  AimRed
+//
+//  Created by Vivek Dhiman on 3/24/16.
+//  Copyright (c) 2016 Vivek Dhiman. All rights reserved.
+//
+
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    cout << "hello world!" << endl;
+    return 0;
+}
